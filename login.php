1bbc510693674fb78a07c8a96ef47b19f5c74ae7
Dieter Meiller <dietermeiller@Dieters-MBP.fritz.box>
2020-05-04 11:30:50 +0200

    style

--- login.php
+++ login.php
@@ -3,11 +3,20 @@
 <a class="btn btn-primary" href="index.html">Overview</a>
 </div>
 <h2>Login</h2>
-<form class="well" action="login.html" method="post">
-	<label>Benutzername:</label>
-	<input name="user" type="text" size="20" maxlength="40"/>
-	<label>Passwort:</label>
-	<input name="pass" type="password" size="20" maxlength="40"/>
-    <input class="btn btn-success" type="submit" value=" Login " />
+<form action="login.html" method="post">
+	<div class="form-group row">
+		<label for="user" class="col-sm-2 col-form-label">Benutzername:</label>
+		<div class="col-sm-10">
+			<input name="user" id="user" type="text" size="20" maxlength="40">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="pass" class="col-sm-2 col-form-label">Passwort:</label>
+		<div class="col-sm-10">
+			<input name="pass" id="pass" type="password" size="20" maxlength="40">
+		</div>
+	</div>
+    <button type='submit'  class='btn btn-success' value='Login'>Login</button>
 </form>
 <?php $end('login') ?>
+
